0f59cf360c2c7572923311d9843c16ea0636c44d
Harshit <hktiwari980@gmail.com>
2025-09-19 20:30:40 +0530

    comments added

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -4,17 +4,20 @@ const File = require("../models/File");
 
 exports.localFileUpload = async (req, res)=>{
      try{
-        //fetch file 
+        //fetch file from request
         const file = req.files.file;
         console.log("File aagyi jee", file);
 
+        // create path where file need to be stored on server
         let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.')[1]}`;
         console.log("PATH->", path);
 
+        //add path to move function
         file.mv(path, (err) =>{
             console.log(err);
         });
 
+        //create a successfull response
         res.json({
             success:true,
             message:"local file Uploaded successfully",
